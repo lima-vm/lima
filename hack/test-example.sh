41b330caaec7a3eb89d79b81f6ab4f403b2347e9
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-06-16 17:26:28 +0900

    CI: more tests
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

---
+++ hack/test-example.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+set -eu -o pipefail
+
+function INFO() {
+	echo "TEST| [INFO] $*"
+}
+
+function WARNING() {
+	echo >&2 "TEST| [WARNING] $*"
+}
+
+function ERROR() {
+	echo >&2 "TEST| [ERROR] $*"
+}
+
+if [[ "${BASH_VERSINFO:-0}" -lt 4 ]]; then
+	ERROR "Bash version is too old: ${BASH_VERSION}"
+	exit 1
+fi
+
+if [ "$#" -ne 1 ]; then
+	ERROR "Usage: $0 FILE.yaml"
+	exit 1
+fi
+
+FILE="$1"
+NAME="$(basename -s .yaml "$FILE")"
+
+INFO "Validating \"$FILE\""
+limactl validate "$FILE"
+
+declare -A CHECKS=(["systemd"]="1" ["systemd-strict"]="1" ["mount-home"]="1" ["containerd-user"]="1")
+
+case "$NAME" in
+"k3s")
+	ERROR "File \"$FILE\" is not testable with this script"
+	exit 1
+	;;
+"fedora")
+	WARNING "Relaxing systemd tests for fedora (For avoiding CI faillure)"
+	# CI failure:
+	# ● run-r2b459797f5b04262bfa79984077a65c7.service                                       loaded failed failed    /usr/bin/systemctl start man-db-cache-update
+	CHECKS["systemd-strict"]=
+	;;
+esac
+
+if limactl ls -q | grep -q "$NAME"; then
+	ERROR "Instance $NAME already exists"
+	exit 1
+fi
+
+INFO "Starting \"$NAME\" from \"$FILE\""
+trap 'limactl delete -f $NAME' EXIT
+set -x
+if ! limactl start --tty=false "$FILE"; then
+  ERROR "Failed to start \"$NAME\""
+  tail "$HOME/.lima/${NAME}"/*.log
+  exit 1
+fi
+
+limactl shell "$NAME" uname -a
+
+limactl shell "$NAME" cat /etc/os-release
+set +x
+
+if [[ -n "${CHECKS["systemd"]}" ]]; then
+	set -x
+	if ! limactl shell "$NAME" systemctl is-system-running --wait; then
+		ERROR "\"systemctl is-system-running\" failed"
+		limactl shell "$NAME" systemctl
+		if [[ -z "${CHECKS["systemd-strict"]}" ]]; then
+			INFO "Ignoring \"systemctl is-system-running\" failure"
+		else
+			exit 1
+		fi
+	fi
+	set +x
+fi
+
+if [[ -n "${CHECKS["mount-home"]}" ]]; then
+	hometmp="$HOME/lima-test-tmp"
+	INFO "Testing home access (\"$hometmp\")"
+	rm -rf "$hometmp"
+	mkdir -p "$hometmp"
+	trap 'rm -rf $hometmp' EXIT
+	echo "random-content-${RANDOM}" >"$hometmp/random"
+	expected="$(cat "$hometmp/random")"
+	got="$(limactl shell "$NAME" cat "$hometmp/random")"
+	INFO "$hometmp/random: expected=${expected}, got=${got}"
+	if [ "$got" != "$expected" ]; then
+		ERROR"Home directory is not shared?"
+		exit 1
+	fi
+fi
+
+if [[ -n "${CHECKS["containerd-user"]}" ]]; then
+	INFO "Run a nginx container with port forwarding 127.0.0.1:8080"
+	set -x
+	limactl shell "$NAME" nerdctl info
+	limactl shell "$NAME" sh -ec "nerdctl pull nginx:alpine >/dev/null"
+	limactl shell "$NAME" nerdctl run -d --name nginx -p 127.0.0.1:8080:80 nginx:alpine
+
+	timeout 3m bash -euxc "until curl -f --retry 30 --retry-connrefused http://127.0.0.1:8080; do sleep 3; done"
+
+	limactl shell "$NAME" nerdctl rm -f nginx
+	set +x
+fi
+
+INFO "Stopping \"$NAME\""
+limactl stop "$NAME"
+
+INFO "Deleting \"$NAME\""
+limactl delete "$NAME"
--- hack/validate-dco.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eux -o pipefail
-# Depends on  github.com/vbatts/git-validation"
-git-validation -run DCO || { echo >&2 'ERROR: DCO sign must be added (git commit -a -s --amend)'; exit 1; }
