3a0bb91f591d88496f977fd6cb98fc3d8395c119
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-06-21 20:45:19 +0900

    cidata: update /etc/fuse.conf earlier
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/cidata/cidata.TEMPLATE.d/boot/30-install-packages.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/30-install-packages.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -eux
 
+update_fuse_conf(){
+  # Modify /etc/fuse.conf to allow "-o allow_root"
+  if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ]; then
+    if ! grep -q "^user_allow_other" /etc/fuse.conf ; then
+      echo "user_allow_other" >> /etc/fuse.conf
+    fi
+  fi
+}
+
 # Install minimum dependencies
 if command -v apt-get 2>&1 >/dev/null; then
   DEBIAN_FRONTEND=noninteractive
@@ -8,6 +17,7 @@ if command -v apt-get 2>&1 >/dev/null; then
   apt-get update
   if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ]; then
     apt-get install -y sshfs
+    update_fuse_conf
   fi
   if [ "${LIMA_CIDATA_CONTAINERD_SYSTEM}" = 1 ] || [ "${LIMA_CIDATA_CONTAINERD_USER}" = 1 ]; then
     apt-get install -y iptables
@@ -18,6 +28,7 @@ if command -v apt-get 2>&1 >/dev/null; then
 elif command -v dnf 2>&1 >/dev/null; then
   if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ]; then
     dnf install -y fuse-sshfs
+    update_fuse_conf
   fi
   if [ "${LIMA_CIDATA_CONTAINERD_SYSTEM}" = 1 ] || [ "${LIMA_CIDATA_CONTAINERD_USER}" = 1 ]; then
     dnf install -y iptables
@@ -35,13 +46,8 @@ elif command -v apk 2>&1 >/dev/null; then
       apk update
       apk add sshfs
     fi
+    update_fuse_conf
     modprobe fuse
   fi
 fi
-# Modify /etc/fuse.conf to allow "-o allow_root"
 
-if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ]; then
-  if ! grep -q "^user_allow_other" /etc/fuse.conf ; then
-    echo "user_allow_other" >> /etc/fuse.conf
-  fi
-fi
