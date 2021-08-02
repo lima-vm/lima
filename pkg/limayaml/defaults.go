e8c524a73122a02f951012bbbed9c0df51447720
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2021-08-02 17:38:33 +0900

    Put machineID to the MAC address hasher
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/limayaml/defaults.go
+++ pkg/limayaml/defaults.go
@@ -8,11 +8,11 @@ import (
 	"strconv"
 
 	"github.com/AkihiroSuda/lima/pkg/guestagent/api"
+	"github.com/AkihiroSuda/lima/pkg/osutil"
 )
 
 func MACAddress(uniqueID string) string {
-	// TODO: combine the uniqueID with the host machineID to create a globally unique hash
-	sha := sha256.Sum256([]byte(uniqueID))
+	sha := sha256.Sum256([]byte(osutil.MachineID() + uniqueID))
 	// According to https://gitlab.com/wireshark/wireshark/-/blob/master/manuf
 	// no well-known MAC addresses start with 0x22.
 	hw := append(net.HardwareAddr{0x22}, sha[0:5]...)
---
+++ pkg/osutil/machineid.go
@@ -0,0 +1,91 @@
+package osutil
+
+import (
+	"bytes"
+	"encoding/xml"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"runtime"
+	"strings"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	machineIDCached string
+	machineIDOnce   sync.Once
+)
+
+func MachineID() string {
+	machineIDOnce.Do(func() {
+		x, err := machineID()
+		if err == nil && x != "" {
+			machineIDCached = x
+			return
+		}
+		logrus.WithError(err).Debug("failed to get machine ID, falling back to use hostname instead")
+		hostname, err := os.Hostname()
+		if err != nil {
+			panic(err)
+		}
+		machineIDCached = hostname
+	})
+	return machineIDCached
+}
+
+func machineID() (string, error) {
+	if runtime.GOOS == "darwin" {
+		ioPlatformExpertDeviceCmd := exec.Command("/usr/sbin/ioreg", "-a", "-d2", "-c", "IOPlatformExpertDevice")
+		ioPlatformExpertDevice, err := ioPlatformExpertDeviceCmd.CombinedOutput()
+		if err != nil {
+			return "", err
+		}
+		return parseIOPlatformUUIDFromIOPlatformExpertDevice(bytes.NewReader(ioPlatformExpertDevice))
+	}
+
+	candidates := []string{
+		"/etc/machine-id",
+		"/var/lib/dbus/machine-id",
+		// We don't use "/sys/class/dmi/id/product_uuid"
+	}
+	for _, f := range candidates {
+		b, err := os.ReadFile(f)
+		if err == nil {
+			return strings.TrimSpace(string(b)), nil
+		}
+	}
+	return "", fmt.Errorf("no machine-id found, tried %v", candidates)
+}
+
+func parseIOPlatformUUIDFromIOPlatformExpertDevice(r io.Reader) (string, error) {
+	d := xml.NewDecoder(r)
+	var (
+		elem            string
+		elemKeyCharData string
+	)
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return "", err
+		}
+		switch v := tok.(type) {
+		case xml.StartElement:
+			elem = v.Name.Local
+		case xml.EndElement:
+			elem = ""
+			if v.Name.Local != "key" {
+				elemKeyCharData = ""
+			}
+		case xml.CharData:
+			if elem == "string" && elemKeyCharData == "IOPlatformUUID" {
+				return string(v), nil
+			}
+			if elem == "key" {
+				elemKeyCharData = string(v)
+			}
+		}
+	}
+}
