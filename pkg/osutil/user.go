acc453c47a036d7bf7f575d01c875c587e653364
Jan Dubois <jan.dubois@suse.com>
2021-09-14 22:42:31 -0700

    Map usernames not allowed by useradd to the fallbackUser "lima"
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- pkg/osutil/user.go
+++ pkg/osutil/user.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 	"github.com/sirupsen/logrus"
 	"os/user"
+	"regexp"
 	"sync"
-
-	"github.com/containerd/containerd/identifiers"
 )
 
 const (
@@ -24,9 +23,12 @@ func LimaUser(warn bool) (*user.User, error) {
 	cache.Do(func() {
 		cache.u, cache.err = user.Current()
 		if cache.err == nil {
-			if err := identifiers.Validate(cache.u.Username); err != nil {
-				cache.warning = fmt.Sprintf("local user %q is not a valid Linux username: %v; using %q username instead",
-					cache.u.Username, err, fallbackUser)
+			// `useradd` only allows user and group names matching the following pattern:
+			// (it allows a trailing '$', but it feels prudent to map those to the fallback user as well)
+			validName := "^[a-z_][a-z0-9_-]*$"
+			if !regexp.MustCompile(validName).Match([]byte(cache.u.Username)) {
+				cache.warning = fmt.Sprintf("local user %q is not a valid Linux username (must match %q); using %q username instead",
+					cache.u.Username, validName, fallbackUser)
 				cache.u.Username = fallbackUser
 			}
 		}
