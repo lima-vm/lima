e579c88507759e983e1048c845829d333201dd75
Anders F Björklund <anders.f.bjorklund@gmail.com>
2022-07-22 09:50:42 +0200

    Add AES detection for Windows using cpuid
    Avoids annoying warning about detection failed
    
    Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

--- pkg/sshutil/sshutil_windows_arm64.go
+++ pkg/sshutil/sshutil_windows_arm64.go
@@ -1,16 +1,11 @@
-//go:build !darwin && !linux && !windows
-// +build !darwin,!linux,!windows
-
 package sshutil
 
 import (
-	"runtime"
-
 	"github.com/sirupsen/logrus"
 )
 
 func detectAESAcceleration() bool {
-	const fallback = runtime.GOARCH == "amd64"
+	const fallback = false
 	logrus.Warnf("cannot detect whether AES accelerator is available, assuming %v", fallback)
 	return fallback
 }
