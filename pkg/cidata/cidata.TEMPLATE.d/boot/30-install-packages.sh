f90c2a8688f36d9b5c1bcd3056f803e6ff69713f
Georg Grauberger <georg.grauberger@gmail.com>
2022-07-24 00:17:14 +0300

    Improve Linux distribution detection
    Signed-off-by: Georg Grauberger <georg.grauberger@gmail.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/30-install-packages.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/30-install-packages.sh
@@ -1,29 +1,86 @@
 #!/bin/sh
 set -eux
 
-update_fuse_conf() {
-	# Modify /etc/fuse.conf (/etc/fuse3.conf) to allow "-o allow_root"
-	if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ]; then
-		fuse_conf="/etc/fuse.conf"
-		if [ -e /etc/fuse3.conf ]; then
-			fuse_conf="/etc/fuse3.conf"
-		fi
-		if ! grep -q "^user_allow_other" "${fuse_conf}"; then
-			echo "user_allow_other" >>"${fuse_conf}"
-		fi
+source /etc/os-release
+SETUP_DNS=0
+DISTRIBUTION=""
+INSTALL_IPTABLES=0
+
+
+main() {
+	determine_distribution
+	determine_need_for_iptables
+	install_minimal_dependencies
+	setup_dns
+	# update_fuse_conf has to be called after installing all the packages,
+	# otherwise apt-get fails with conflict	
+	update_fuse_conf	
+}
+
+determine_distribution() {
+	if command -v apt-get >/dev/null 2>&1 && [ "$ID_LIKE" = "debian" ]; then
+		DISTRIBUTION="debian_like"
+	elif command -v dnf >/dev/null 2>&1 && [ "$ID" = "fedora" ]; then
+		DISTRIBUTION="redhat_like"
+	elif command -v yum >/dev/null 2>&1 && [ "$ID_LIKE" = "centos" ]; then
+		DISTRIBUTION="centos_like"
+	elif command -v pacman >/dev/null 2>&1 && [ "$ID_LIKE" = "arch" ]; then
+		DISTRIBUTION="arch_like"
+	elif command -v zypper >/dev/null 2>&1 && [ "$ID_LIKE" = "opensuse suse" ]; then
+		DISTRIBUTION="suse_like"
+	elif command -v apk >/dev/null 2>&1 && [ "ID" = "alpine" ]; then
+		DISTRIBUTION="alpine_like"
+	else
+		guess_distribution_based_on_package_manager
 	fi
 }
 
-INSTALL_IPTABLES=0
-if [ "${LIMA_CIDATA_CONTAINERD_SYSTEM}" = 1 ] || [ "${LIMA_CIDATA_CONTAINERD_USER}" = 1 ]; then
-	INSTALL_IPTABLES=1
-fi
-if [ "${LIMA_CIDATA_UDP_DNS_LOCAL_PORT}" -ne 0 ] || [ "${LIMA_CIDATA_TCP_DNS_LOCAL_PORT}" -ne 0 ]; then
-	INSTALL_IPTABLES=1
-fi
+guess_distribution_based_on_package_manager() {
+	if command -v apt-get >/dev/null 2>&1; then
+		DISTRIBUTION="debian_like"
+	elif command -v dnf >/dev/null 2>&1; then
+		DISTRIBUTION="redhat_like"
+	elif command -v yum >/dev/null 2>&1; then
+		DISTRIBUTION="redhat_like"
+	elif command -v pacman >/dev/null 2>&1; then
+		DISTRIBUTION="arch_like"
+	elif command -v zypper >/dev/null 2>&1; then
+		DISTRIBUTION="suse_like"
+	elif command -v apk >/dev/null 2>&1; then
+		DISTRIBUTION="alpine_like"
+	fi
+}
+
+determine_need_for_iptables() {
+	if [ "${LIMA_CIDATA_CONTAINERD_SYSTEM}" = 1 ] || [ "${LIMA_CIDATA_CONTAINERD_USER}" = 1 ]; then
+		INSTALL_IPTABLES=1
+	fi
+	if [ "${LIMA_CIDATA_UDP_DNS_LOCAL_PORT}" -ne 0 ] || [ "${LIMA_CIDATA_TCP_DNS_LOCAL_PORT}" -ne 0 ]; then
+		INSTALL_IPTABLES=1
+	fi
+}
+
+install_minimal_dependencies() {
+	case $DISTRIBUTION in
+		"debian_like") install_debian_dependencies
+		;;
+		"redhat_like") install_redhat_dependencies
+		;;
+		"centos_like") install_centos_dependencies
+		;;
+		"arch_like") install_arch_dependencies
+		;;
+		"suse_like") install_suse_dependencies
+		;;
+		"alpine_like") install_alpine_dependencies
+		;;
+		*) echo "Could not determine any suitable actions to provision machine"
+		;;
+	esac
+}
 
 # Install minimum dependencies
-if command -v apt-get >/dev/null 2>&1; then
+install_debian_dependencies() {
 	pkgs=""
 	if [ "${LIMA_CIDATA_MOUNTTYPE}" = "reverse-sshfs" ]; then
 		if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ] && ! command -v sshfs >/dev/null 2>&1; then
@@ -43,7 +100,9 @@ if command -v apt-get >/dev/null 2>&1; then
 		# shellcheck disable=SC2086
 		apt-get install -y --no-upgrade --no-install-recommends -q ${pkgs}
 	fi
-elif command -v dnf >/dev/null 2>&1; then
+}
+
+install_redhat_dependencies() {
 	pkgs=""
 	if ! command -v tar >/dev/null 2>&1; then
 		pkgs="${pkgs} tar"
@@ -84,7 +143,9 @@ elif command -v dnf >/dev/null 2>&1; then
 		# Workaround for https://github.com/containerd/stargz-snapshotter/issues/340
 		ln -s fusermount3 /usr/bin/fusermount
 	fi
-elif command -v yum >/dev/null 2>&1; then
+}
+
+install_centos_dependencies() {
 	echo "DEPRECATED: CentOS7 and others RHEL-like version 7 are unsupported and might be removed or stop to work in future lima releases"
 	pkgs=""
 	yum_install_flags="-y"
@@ -113,7 +174,9 @@ elif command -v yum >/dev/null 2>&1; then
 		yum install ${yum_install_flags} ${pkgs}
 		yum-config-manager --disable epel >/dev/null 2>&1
 	fi
-elif command -v pacman >/dev/null 2>&1; then
+}
+
+install_arch_dependencies() {
 	pkgs=""
 	if [ "${LIMA_CIDATA_MOUNTTYPE}" = "reverse-sshfs" ]; then
 		if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ] && ! command -v sshfs >/dev/null 2>&1; then
@@ -125,7 +188,9 @@ elif command -v pacman >/dev/null 2>&1; then
 		# shellcheck disable=SC2086
 		pacman -Sy --noconfirm ${pkgs}
 	fi
-elif command -v zypper >/dev/null 2>&1; then
+}
+
+install_suse_dependencies() {
 	pkgs=""
 	if [ "${LIMA_CIDATA_MOUNTTYPE}" = "reverse-sshfs" ]; then
 		if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ] && ! command -v sshfs >/dev/null 2>&1; then
@@ -142,7 +207,9 @@ elif command -v zypper >/dev/null 2>&1; then
 		# shellcheck disable=SC2086
 		zypper --non-interactive install -y --no-recommends ${pkgs}
 	fi
-elif command -v apk >/dev/null 2>&1; then
+}
+
+install_alpine_dependencies() {
 	pkgs=""
 	if [ "${LIMA_CIDATA_MOUNTTYPE}" = "reverse-sshfs" ]; then
 		if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ] && ! command -v sshfs >/dev/null 2>&1; then
@@ -157,22 +224,32 @@ elif command -v apk >/dev/null 2>&1; then
 		# shellcheck disable=SC2086
 		apk add ${pkgs}
 	fi
-fi
+}
 
-SETUP_DNS=0
-if [ -n "${LIMA_CIDATA_UDP_DNS_LOCAL_PORT}" ] && [ "${LIMA_CIDATA_UDP_DNS_LOCAL_PORT}" -ne 0 ]; then
-	SETUP_DNS=1
-fi
-if [ -n "${LIMA_CIDATA_TCP_DNS_LOCAL_PORT}" ] && [ "${LIMA_CIDATA_TCP_DNS_LOCAL_PORT}" -ne 0 ]; then
-	SETUP_DNS=1
-fi
-if [ "${SETUP_DNS}" = 1 ]; then
-	# Try to setup iptables rule again, in case we just installed iptables
-	"${LIMA_CIDATA_MNT}/boot/09-host-dns-setup.sh"
-fi
+setup_dns() {
+	if [ -n "${LIMA_CIDATA_UDP_DNS_LOCAL_PORT}" ] && [ "${LIMA_CIDATA_UDP_DNS_LOCAL_PORT}" -ne 0 ]; then
+		SETUP_DNS=1
+	fi
+	if [ -n "${LIMA_CIDATA_TCP_DNS_LOCAL_PORT}" ] && [ "${LIMA_CIDATA_TCP_DNS_LOCAL_PORT}" -ne 0 ]; then
+		SETUP_DNS=1
+	fi
+	if [ "${SETUP_DNS}" = 1 ]; then
+		# Try to setup iptables rule again, in case we just installed iptables
+		"${LIMA_CIDATA_MNT}/boot/09-host-dns-setup.sh"
+	fi
+}
 
-# update_fuse_conf has to be called after installing all the packages,
-# otherwise apt-get fails with conflict
-if [ "${LIMA_CIDATA_MOUNTTYPE}" = "reverse-sshfs" ]; then
-	update_fuse_conf
-fi
+update_fuse_conf() {
+	# Modify /etc/fuse.conf (/etc/fuse3.conf) to allow "-o allow_root"
+	if [ "${LIMA_CIDATA_MOUNTS}" -gt 0 ] && [ "${LIMA_CIDATA_MOUNTTYPE}" = "reverse-sshfs" ]; then
+		fuse_conf="/etc/fuse.conf"
+		if [ -e /etc/fuse3.conf ]; then
+			fuse_conf="/etc/fuse3.conf"
+		fi
+		if ! grep -q "^user_allow_other" "${fuse_conf}"; then
+			echo "user_allow_other" >>"${fuse_conf}"
+		fi
+	fi
+}
+
+main
