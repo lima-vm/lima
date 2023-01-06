lima-vm/lima

commit 75594fa33bd73a2cc80a389993c7573b4bf3ec58

add inspect CLI option
Signed-off-by: Avi Deitcher <avi@deitcher.net>

pkg/store/instance.go
@@ -2,18 +2,23 @@ package store
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
+	"io"
 	"os"
+	"os/user"
 	"path/filepath"
 	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
+	"text/tabwriter"
 	"text/template"
 	"time"
 
 	"github.com/docker/go-units"
+	"github.com/goccy/go-yaml"
 	hostagentclient "github.com/lima-vm/lima/pkg/hostagent/api/client"
 	"github.com/lima-vm/lima/pkg/limayaml"
 	"github.com/lima-vm/lima/pkg/store/dirnames"
@@ -46,6 +51,7 @@ type Instance struct {
 	HostAgentPID    int                `json:"hostAgentPID,omitempty"`
 	DriverPID       int                `json:"driverPID,omitempty"`
 	Errors          []error            `json:"errors,omitempty"`
+	Config          *limayaml.LimaYAML `json:"config,omitempty"`
 }
 
 func (inst *Instance) LoadYAML() (*limayaml.LimaYAML, error) {
@@ -79,6 +85,7 @@ func Inspect(instName string) (*Instance, error) {
 		inst.Errors = append(inst.Errors, err)
 		return inst, nil
 	}
+	inst.Config = y
 	inst.Arch = *y.Arch
 	inst.VMType = *y.VMType
 	inst.CPUType = y.CPUType[*y.Arch]
@@ -224,4 +231,65 @@ func AddGlobalFields(inst *Instance) (FormatData, error) {
 		return FormatData{}, err
 	}
 	return data, nil
+}
+
+// PrintInstances prints instances in a requested format to a given io.Writer.
+// Supported formats are "json", "yaml", "table", or a go template
+func PrintInstances(w io.Writer, instances []*Instance, format string) error {
+	switch format {
+	case "json":
+		b, err := json.Marshal(instances)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, string(b))
+	case "yaml":
+		b, err := yaml.Marshal(instances)
+		if err != nil {
+			return err
+		}
+		fmt.Fprintln(w, string(b))
+	case "table":
+		w := tabwriter.NewWriter(w, 4, 8, 4, ' ', 0)
+		fmt.Fprintln(w, "NAME\tSTATUS\tSSH\tVMTYPE\tARCH\tCPUS\tMEMORY\tDISK\tDIR")
+
+		u, err := user.Current()
+		if err != nil {
+			return err
+		}
+		homeDir := u.HomeDir
+
+		for _, instance := range instances {
+			dir := instance.Dir
+			if strings.HasPrefix(dir, homeDir) {
+				dir = strings.Replace(dir, homeDir, "~", 1)
+			}
+			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%d\t%s\t%s\t%s\n",
+				instance.Name,
+				instance.Status,
+				fmt.Sprintf("127.0.0.1:%d", instance.SSHLocalPort),
+				instance.VMType,
+				instance.Arch,
+				instance.CPUs,
+				units.BytesSize(float64(instance.Memory)),
+				units.BytesSize(float64(instance.Disk)),
+				dir,
+			)
+		}
+		return w.Flush()
+	default:
+		tmpl, err := template.New("format").Parse(format)
+		if err != nil {
+			return fmt.Errorf("invalid go template: %w", err)
+		}
+		for _, instance := range instances {
+			err = tmpl.Execute(w, instance)
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(w)
+		}
+		return nil
+	}
+	return nil
 }

cmd/limactl/inspect.go
@@ -1,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/lima-vm/lima/pkg/store"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+)
+
+func newInspectCommand() *cobra.Command {
+	inspectCommand := &cobra.Command{
+		Use:   "inspect [flags] [INSTANCE]...",
+		Short: "Inspect instances of Lima.",
+		Long: `Inspect instances of Lima.
+		Includes both the current state, as well as the combined configuration used to create the instance.
+		The output can be presented in one of several formats, using the --format <format> flag.
+		
+		--format json  - output in json format
+		--format yaml  - output in yaml format
+		--format table - output in table format
+		--format '{{ <go template> }}' - if the format begins and ends with '{{ }}', then it is used as a go template.
+	  `,
+		Args:              cobra.ArbitraryArgs,
+		RunE:              inspectAction,
+		ValidArgsFunction: inspectBashComplete,
+	}
+
+	inspectCommand.Flags().StringP("format", "f", "json", "output format, one of: 'json', 'yaml', a go-template")
+
+	return inspectCommand
+}
+
+func inspectAction(cmd *cobra.Command, args []string) error {
+	format, err := cmd.Flags().GetString("format")
+	if err != nil {
+		return err
+	}
+
+	allinstances, err := store.Instances()
+	if err != nil {
+		return err
+	}
+
+	instanceNames := []string{}
+	if len(args) > 0 {
+		for _, arg := range args {
+			matches := instanceMatches(arg, allinstances)
+			if len(matches) > 0 {
+				instanceNames = append(instanceNames, matches...)
+			} else {
+				logrus.Warnf("No instance matching %v found.", arg)
+			}
+		}
+	} else {
+		instanceNames = allinstances
+	}
+
+	// get the state and config for all the requested instances
+	var instances []*store.Instance
+	for _, instanceName := range instanceNames {
+		instance, err := store.Inspect(instanceName)
+		if err != nil {
+			return fmt.Errorf("unable to load instance %s: %w", instanceName, err)
+		}
+		instances = append(instances, instance)
+	}
+
+	return store.PrintInstances(cmd.OutOrStdout(), instances, format)
+}
+
+func inspectBashComplete(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	return bashCompleteInstanceNames(cmd)
+}

cmd/limactl/list.go
@@ -1,17 +1,12 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"fmt"
-	"os/user"
 	"reflect"
 	"sort"
 	"strings"
-	"text/tabwriter"
-	"text/template"
 
-	"github.com/docker/go-units"
 	"github.com/lima-vm/lima/pkg/store"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
@@ -36,15 +31,26 @@ func fieldNames() []string {
 
 func newListCommand() *cobra.Command {
 	listCommand := &cobra.Command{
-		Use:               "list [flags] [INSTANCE]...",
-		Aliases:           []string{"ls"},
-		Short:             "List instances of Lima.",
+		Use:     "list [flags] [INSTANCE]...",
+		Aliases: []string{"ls"},
+		Short:   "List instances of Lima.",
+		Long: `List instances of Lima.
+		The output can be presented in one of several formats, using the --format <format> flag.
+		
+		  --format json  - output in json format
+		  --format yaml  - output in yaml format
+		  --format table - output in table format
+		  --format '{{ <go template> }}' - if the format begins and ends with '{{ }}', then it is used as a go template.
+
+		The following legacy flags continue to function:
+		  --json - equal to '--format json'
+		`,
 		Args:              cobra.ArbitraryArgs,
 		RunE:              listAction,
 		ValidArgsFunction: listBashComplete,
 	}
 
-	listCommand.Flags().StringP("format", "f", "", "Format the output using the given Go template")
+	listCommand.Flags().StringP("format", "f", "table", "output format, one of: json, yaml, table, go-template")
 	listCommand.Flags().Bool("list-fields", false, "List fields available for format")
 	listCommand.Flags().Bool("json", false, "JSONify output")
 	listCommand.Flags().BoolP("quiet", "q", false, "Only show names")
@@ -67,7 +73,7 @@ func listAction(cmd *cobra.Command, args []string) error {
 	if err != nil {
 		return err
 	}
-	goFormat, err := cmd.Flags().GetString("format")
+	format, err := cmd.Flags().GetString("format")
 	if err != nil {
 		return err
 	}
@@ -80,131 +86,76 @@ func listAction(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	if goFormat != "" && listFields {
-		return errors.New("option --format conflicts with --list-fields")
+	if jsonFormat {
+		format = "json"
 	}
-	if jsonFormat && listFields {
-		return errors.New("option --json conflicts with --list-fields")
+
+	// conflicts
+	if jsonFormat && cmd.Flags().Changed("format") {
+		return errors.New("option --json conflicts with option --format")
 	}
+	if listFields && cmd.Flags().Changed("format") {
+		return errors.New("option --list-fields conflicts with option --format")
+	}
+
+	if quiet && format != "table" {
+		return errors.New("option --quiet can only be used with '--format table'")
+	}
+
 	if listFields {
 		names := fieldNames()
 		sort.Strings(names)
 		fmt.Println(strings.Join(names, "\n"))
 		return nil
 	}
-	if quiet && jsonFormat {
-		return errors.New("option --quiet conflicts with --json")
-	}
-	if goFormat != "" && jsonFormat {
-		return errors.New("option --format conflicts with --json")
-	}
 
 	allinstances, err := store.Instances()
 	if err != nil {
 		return err
 	}
+	if len(allinstances) == 0 {
+		logrus.Warn("No instance found. Run `limactl start` to create an instance.")
+		return nil
+	}
 
-	instances := []string{}
+	instanceNames := []string{}
 	if len(args) > 0 {
 		for _, arg := range args {
 			matches := instanceMatches(arg, allinstances)
 			if len(matches) > 0 {
-				instances = append(instances, matches...)
+				instanceNames = append(instanceNames, matches...)
 			} else {
 				logrus.Warnf("No instance matching %v found.", arg)
 			}
 		}
 	} else {
-		instances = allinstances
+		instanceNames = allinstances
 	}
 
 	if quiet {
-		for _, instName := range instances {
+		for _, instName := range instanceNames {
 			fmt.Fprintln(cmd.OutOrStdout(), instName)
 		}
 		return nil
 	}
 
-	if goFormat != "" {
-		tmpl, err := template.New("format").Parse(goFormat)
+	// get the state and config for all the requested instances
+	var instances []*store.Instance
+	for _, instanceName := range instanceNames {
+		instance, err := store.Inspect(instanceName)
 		if err != nil {
-			return err
+			return fmt.Errorf("unable to load instance %s: %w", instanceName, err)
 		}
-		for _, instName := range instances {
-			inst, err := store.Inspect(instName)
-			if err != nil {
-				logrus.WithError(err).Errorf("instance %q does not exist?", instName)
-				continue
-			}
-			data, err := store.AddGlobalFields(inst)
-			if err != nil {
-				logrus.WithError(err).Error("Cannot add global fields to instance data")
-				continue
-			}
-			err = tmpl.Execute(cmd.OutOrStdout(), data)
-			if err != nil {
-				return err
-			}
-			fmt.Fprintln(cmd.OutOrStdout())
-		}
-		return nil
-	}
-	if jsonFormat {
-		for _, instName := range instances {
-			inst, err := store.Inspect(instName)
-			if err != nil {
-				logrus.WithError(err).Errorf("instance %q does not exist?", instName)
-				continue
-			}
-			b, err := json.Marshal(inst)
-			if err != nil {
-				return err
-			}
-			fmt.Fprintln(cmd.OutOrStdout(), string(b))
-		}
-		return nil
+		instances = append(instances, instance)
 	}
 
-	w := tabwriter.NewWriter(cmd.OutOrStdout(), 4, 8, 4, ' ', 0)
-	fmt.Fprintln(w, "NAME\tSTATUS\tSSH\tVMTYPE\tARCH\tCPUS\tMEMORY\tDISK\tDIR")
-
-	if len(allinstances) == 0 {
-		logrus.Warn("No instance found. Run `limactl start` to create an instance.")
+	for _, instance := range instances {
+		if len(instance.Errors) > 0 {
+			logrus.WithField("errors", instance.Errors).Warnf("instance %q has errors", instance.Name)
+		}
 	}
 
-	u, err := user.Current()
-	if err != nil {
-		return err
-	}
-	homeDir := u.HomeDir
-
-	for _, instName := range instances {
-		inst, err := store.Inspect(instName)
-		if err != nil {
-			logrus.WithError(err).Errorf("instance %q does not exist?", instName)
-			continue
-		}
-		if len(inst.Errors) > 0 {
-			logrus.WithField("errors", inst.Errors).Warnf("instance %q has errors", instName)
-		}
-		dir := inst.Dir
-		if strings.HasPrefix(dir, homeDir) {
-			dir = strings.Replace(dir, homeDir, "~", 1)
-		}
-		fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t%d\t%s\t%s\t%s\n",
-			inst.Name,
-			inst.Status,
-			fmt.Sprintf("127.0.0.1:%d", inst.SSHLocalPort),
-			inst.VMType,
-			inst.Arch,
-			inst.CPUs,
-			units.BytesSize(float64(inst.Memory)),
-			units.BytesSize(float64(inst.Disk)),
-			dir,
-		)
-	}
-
-	return w.Flush()
+	return store.PrintInstances(cmd.OutOrStdout(), instances, format)
 }
 
 func listBashComplete(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {

cmd/limactl/main.go
@@ -93,6 +93,7 @@ func newApp() *cobra.Command {
 		newEditCommand(),
 		newFactoryResetCommand(),
 		newDiskCommand(),
+		newInspectCommand(),
 	)
 	return rootCmd
 }

commit 820b78761946dd4b3902536e98ec949c876e0fc3

Make sure `limactl list --json` to print line-delimited JSON
Fix issue 1280
A regression in 75594fa33bd73a2cc80a389993c7573b4bf3ec58 (PR 1249)

Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

pkg/store/instance.go
@@ -2,7 +2,6 @@ package store
 
 import (
 	"context"
-	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
@@ -18,11 +17,11 @@ import (
 	"time"
 
 	"github.com/docker/go-units"
-	"github.com/goccy/go-yaml"
 	hostagentclient "github.com/lima-vm/lima/pkg/hostagent/api/client"
 	"github.com/lima-vm/lima/pkg/limayaml"
 	"github.com/lima-vm/lima/pkg/store/dirnames"
 	"github.com/lima-vm/lima/pkg/store/filenames"
+	"github.com/lima-vm/lima/pkg/textutil"
 )
 
 type Status = string
@@ -238,17 +237,9 @@ func AddGlobalFields(inst *Instance) (FormatData, error) {
 func PrintInstances(w io.Writer, instances []*Instance, format string) error {
 	switch format {
 	case "json":
-		b, err := json.Marshal(instances)
-		if err != nil {
-			return err
-		}
-		fmt.Fprintln(w, string(b))
+		format = "{{json .}}"
 	case "yaml":
-		b, err := yaml.Marshal(instances)
-		if err != nil {
-			return err
-		}
-		fmt.Fprintln(w, string(b))
+		format = "{{yaml .}}"
 	case "table":
 		w := tabwriter.NewWriter(w, 4, 8, 4, ' ', 0)
 		fmt.Fprintln(w, "NAME\tSTATUS\tSSH\tVMTYPE\tARCH\tCPUS\tMEMORY\tDISK\tDIR")
@@ -278,18 +269,18 @@ func PrintInstances(w io.Writer, instances []*Instance, format string) error {
 		}
 		return w.Flush()
 	default:
-		tmpl, err := template.New("format").Parse(format)
+		// NOP
+	}
+	tmpl, err := template.New("format").Funcs(textutil.TemplateFuncMap).Parse(format)
+	if err != nil {
+		return fmt.Errorf("invalid go template: %w", err)
+	}
+	for _, instance := range instances {
+		err = tmpl.Execute(w, instance)
 		if err != nil {
-			return fmt.Errorf("invalid go template: %w", err)
+			return err
 		}
-		for _, instance := range instances {
-			err = tmpl.Execute(w, instance)
-			if err != nil {
-				return err
-			}
-			fmt.Fprintln(w)
-		}
-		return nil
+		fmt.Fprintln(w)
 	}
 	return nil
 }

pkg/textutil/textutil.go
@@ -2,7 +2,12 @@ package textutil
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
+	"strings"
 	"text/template"
+
+	"github.com/goccy/go-yaml"
 )
 
 // ExecuteTemplate executes a text/template template.
@@ -16,4 +21,25 @@ func ExecuteTemplate(tmpl string, args interface{}) ([]byte, error) {
 		return nil, err
 	}
 	return b.Bytes(), nil
+}
+
+// TemplateFuncMap is a text/template FuncMap.
+var TemplateFuncMap = template.FuncMap{
+	"json": func(v interface{}) string {
+		var b bytes.Buffer
+		enc := json.NewEncoder(&b)
+		enc.SetEscapeHTML(false)
+		if err := enc.Encode(v); err != nil {
+			panic(fmt.Errorf("failed to marshal as JSON: %+v: %w", v, err))
+		}
+		return strings.TrimSuffix(b.String(), "\n")
+	},
+	"yaml": func(v interface{}) string {
+		var b bytes.Buffer
+		enc := yaml.NewEncoder(&b)
+		if err := enc.Encode(v); err != nil {
+			panic(fmt.Errorf("failed to marshal as YAML: %+v: %w", v, err))
+		}
+		return "---\n" + strings.TrimSuffix(b.String(), "\n")
+	},
 }

pkg/textutil/textutil_test.go
@@ -1,0 +1,32 @@
+package textutil
+
+import (
+	"bytes"
+	"testing"
+	"text/template"
+
+	"gotest.tools/v3/assert"
+)
+
+func TestTemplateFuncs(t *testing.T) {
+	type X struct {
+		Foo int    `json:"foo" yaml:"foo"`
+		Bar string `json:"bar" yaml:"bar"`
+	}
+	x := X{Foo: 42, Bar: "hello"}
+
+	testCases := map[string]string{
+		"{{json .}}": `{"foo":42,"bar":"hello"}`,
+		"{{yaml .}}": `---
+foo: 42
+bar: hello`,
+	}
+
+	for format, expected := range testCases {
+		tmpl, err := template.New("format").Funcs(TemplateFuncMap).Parse(format)
+		assert.NilError(t, err)
+		var b bytes.Buffer
+		assert.NilError(t, tmpl.Execute(&b, x))
+		assert.Equal(t, expected, b.String())
+	}
+}