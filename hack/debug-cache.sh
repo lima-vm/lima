ee9c332aa2200789f33ad5dda12717ab08d6bd28
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2023-08-03 20:52:50 +0900

    CI: debug cache
    Debug issue 1711
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

---
+++ hack/debug-cache.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -eu -o pipefail
+cache_dir="${HOME}/Library/Caches"
+if [ "$(uname -s)" != "Darwin" ]; then
+	cache_dir="${HOME}/.cache"
+fi
+if [ ! -e "${cache_dir}/lima" ]; then
+	echo "No cache"
+	exit 0
+fi
+for f in "${cache_dir}/lima/download/by-url-sha256/"*; do
+	echo "$f"
+	ls -l "$f"
+	cat "${f}/url"
+	echo
+	echo ---
+done
