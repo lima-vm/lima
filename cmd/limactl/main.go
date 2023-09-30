92cdf5eb5a2fd72aa0afeebe88396f51b3d8cdd1
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2023-09-30 16:09:36 +0900

    limactl: make `--tty` to be a global flag
    `limactl start --tty=false` can be now `limactl --tty=false start` .
    
    The previous form continues to work too.
    
    Fix issue 1861
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- cmd/limactl/main.go
+++ cmd/limactl/main.go
@@ -58,6 +58,8 @@ func newApp() *cobra.Command {
 	}
 	rootCmd.PersistentFlags().String("log-level", "", "Set the logging level [trace, debug, info, warn, error]")
 	rootCmd.PersistentFlags().Bool("debug", false, "debug mode")
+	// TODO: "survey" does not support using cygwin terminal on windows yet
+	rootCmd.PersistentFlags().Bool("tty", isatty.IsTerminal(os.Stdout.Fd()), "Enable TUI interactions such as opening an editor. Defaults to true when stdout is a terminal. Set to false for automation.")
 	rootCmd.PersistentPreRunE = func(cmd *cobra.Command, args []string) error {
 		l, _ := cmd.Flags().GetString("log-level")
 		if l != "" {
