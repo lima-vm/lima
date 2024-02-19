7e0d03f454f7f61f9a4d675bab93c89d09b79a87
Anders F Björklund <anders.f.bjorklund@gmail.com>
2024-02-19 21:29:37 +0100

    Support /etc/ssh/sshd_config.d for AcceptEnv
    Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/11-colorterm-environment.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/11-colorterm-environment.sh
@@ -1,18 +1,24 @@
 #!/bin/sh
 set -eux
 
-# openSUSE Tumbleweed uses /etc/ssh/sshd_config.d, not /etc/ssh/sshd_config
-# TODO: support /etc/ssh/sshd_config.d
-if [ ! -e /etc/ssh/sshd_config ]; then
+if [ -d /etc/ssh/sshd_config.d ]; then
+	if [ -e /etc/ssh/sshd_config.d/10-acceptenv-colorterm.conf ]; then
+		exit 0
+	fi
+
+	# accept any incoming COLORTERM environment variable
+	echo "AcceptEnv COLORTERM" >/etc/ssh/sshd_config.d/10-acceptenv-colorterm.conf
+elif [ -e /etc/ssh/sshd_config ]; then
+	if grep -q "COLORTERM" /etc/ssh/sshd_config; then
+		exit 0
+	fi
+
+	# accept any incoming COLORTERM environment variable
+	sed -i 's/^AcceptEnv LANG LC_\*$/AcceptEnv COLORTERM LANG LC_*/' /etc/ssh/sshd_config
+else
 	exit 0
 fi
 
-if grep -q "COLORTERM" /etc/ssh/sshd_config; then
-	exit 0
-fi
-
-# accept any incoming COLORTERM environment variable
-sed -i 's/^AcceptEnv LANG LC_\*$/AcceptEnv COLORTERM LANG LC_*/' /etc/ssh/sshd_config
 if [ -f /sbin/openrc-run ]; then
 	rc-service --ifstarted sshd reload
 elif command -v systemctl >/dev/null 2>&1; then
