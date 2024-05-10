b5c375c1152876c87c80ab410dc59946a4c88986
Anders F Björklund <anders.f.bjorklund@gmail.com>
2024-05-10 11:31:17 +0200

    Add marshal unit test for default.yaml
    Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

--- pkg/limayaml/limayaml_test.go
+++ pkg/limayaml/limayaml_test.go
@@ -1,11 +1,35 @@
 package limayaml
 
 import (
+	"encoding/json"
 	"os"
 	"testing"
 
 	"gotest.tools/v3/assert"
 )
+
+func dumpJSON(d interface{}) string {
+	b, err := json.Marshal(d)
+	if err != nil {
+		return "ERROR"
+	}
+	return string(b)
+}
+
+const emptyYAML = "images: []\n"
+
+func TestEmptyYAML(t *testing.T) {
+	var y LimaYAML
+	t.Log(dumpJSON(y))
+	b, err := marshalYAML(y)
+	assert.NilError(t, err)
+	assert.Equal(t, string(b), emptyYAML)
+}
+
+const defaultYAML = `images: []
+ssh:
+  localPort: 0
+`
 
 func TestDefaultYAML(t *testing.T) {
 	bytes, err := os.ReadFile("default.yaml")
@@ -13,4 +37,10 @@ func TestDefaultYAML(t *testing.T) {
 	var y LimaYAML
 	err = unmarshalYAML(bytes, &y, "")
 	assert.NilError(t, err)
+	y.Images = nil // remove default images
+	y.Mounts = nil // remove default mounts
+	t.Log(dumpJSON(y))
+	b, err := marshalYAML(y)
+	assert.NilError(t, err)
+	assert.Equal(t, string(b), defaultYAML)
 }
