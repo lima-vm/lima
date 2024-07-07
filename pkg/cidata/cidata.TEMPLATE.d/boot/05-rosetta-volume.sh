23026417a888535c84ec26f11a2b9da66068f0ad
Norio Nomura <norio.nomura@gmail.com>
2024-07-07 14:42:53 +0900

    Create `rosetta.conf` for `binfmt.d(5)`
    Sometime `qemu-user-static` interpreters are registered with `systemd-binfmt.service(8)` by creating configuration files for `binfmt.d(5)`.
    By creating a `binfmt.d(5)` configuration for Rosetta, Rosetta will be prioritized over `qemu-user-static` regardless of the execution timing of `systemd-binfmt.service(8)`.
    In subsequent startups, Rosetta will already be registered with binfmt_misc at this stage, so a check has been added to prevent errors due to duplicate registrations.
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/05-rosetta-volume.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/05-rosetta-volume.sh
@@ -29,7 +29,13 @@ else
 fi
 
 if [ "$LIMA_CIDATA_ROSETTA_BINFMT" = "true" ]; then
-	echo \
-		':rosetta:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00:\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/mnt/lima-rosetta/rosetta:OCF' \
-		>/proc/sys/fs/binfmt_misc/register
+	rosetta_binfmt=":rosetta:M::\x7fELF\x02\x01\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x3e\x00:\xff\xff\xff\xff\xff\xfe\xfe\x00\xff\xff\xff\xff\xff\xff\xff\xff\xfe\xff\xff\xff:/mnt/lima-rosetta/rosetta:OCF"
+
+	# If rosetta is not registered in binfmt_misc, register it.
+	[ -f /proc/sys/fs/binfmt_misc/rosetta ] || echo "$rosetta_binfmt" >/proc/sys/fs/binfmt_misc/register
+
+	# Create binfmt.d(5) configuration to prioritize rosetta even if qemu-user-static is installed on systemd based systems.
+	binfmtd_conf=/usr/lib/binfmt.d/rosetta.conf
+	# If the binfmt.d directory exists, consider systemd-binfmt.service(8) to be enabled and create the configuration file.
+	[ ! -d "$(dirname "$binfmtd_conf")" ] || [ -f "$binfmtd_conf" ] || echo "$rosetta_binfmt" >"$binfmtd_conf"
 fi
