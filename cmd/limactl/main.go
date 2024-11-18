1d533a73d1c5ba7f46a192c6a1b3d0869cc66592
Norio Nomura <norio.nomura@gmail.com>
2024-11-18 20:20:49 +0900

    Add `limactl save`
    ```console
    $ limactl save --help
    Save an instance
    
    Usage:
      limactl save INSTANCE [flags]
    
    Flags:
      -h, --help   help for save
    
    Global Flags:
          --debug               debug mode
          --log-format string   Set the logging format [text, json] (default "text")
          --log-level string    Set the logging level [trace, debug, info, warn, error]
          --tty                 Enable TUI interactions such as opening an editor. Defaults to true when stdout is a terminal. Set to false for automation. (default true)
    ```
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>

--- cmd/limactl/main.go
+++ cmd/limactl/main.go
@@ -155,6 +155,7 @@ func newApp() *cobra.Command {
 		newProtectCommand(),
 		newUnprotectCommand(),
 		newTunnelCommand(),
+		newSaveCommand(),
 	)
 	if runtime.GOOS == "darwin" || runtime.GOOS == "linux" {
 		rootCmd.AddCommand(startAtLoginCommand())
