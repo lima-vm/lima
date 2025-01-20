lima-vm/lima.WIP-UNDER-REPO-TRANSFER

Refactor: deduplicate common code into sshutil
Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

cmd/limactl/shell.go
@@ -13,21 +13,16 @@ import (
 	"github.com/lima-vm/lima/pkg/sshutil"
 	"github.com/lima-vm/lima/pkg/store"
 	"github.com/mattn/go-isatty"
-	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
-
-// Environment variable that allows configuring the command (alias) to execute
-// in place of the 'ssh' executable.
-const envShellSSH = "SSH"
 
 const shellHelp = `Execute shell in Lima
 
 lima command is provided as an alias for limactl shell $LIMA_INSTANCE. $LIMA_INSTANCE defaults to "` + DefaultInstanceName + `".
 
 By default, the first 'ssh' executable found in the host's PATH is used to connect to the Lima instance.
-A custom ssh alias can be used instead by setting the $` + envShellSSH + ` environment variable.
+A custom ssh alias can be used instead by setting the $` + sshutil.EnvShellSSH + ` environment variable.
 
 Hint: try --debug to show the detailed logs, if it seems hanging (mostly due to some SSH issue).
 `
@@ -142,28 +137,9 @@ func shellAction(cmd *cobra.Command, args []string) error {
 		)
 	}
 
-	var arg0 string
-	var arg0Args []string
-
-	if sshShell := os.Getenv(envShellSSH); sshShell != "" {
-		sshShellFields, err := shellwords.Parse(sshShell)
-		switch {
-		case err != nil:
-			logrus.WithError(err).Warnf("Failed to split %s variable into shell tokens. "+
-				"Falling back to 'ssh' command", envShellSSH)
-		case len(sshShellFields) > 0:
-			arg0 = sshShellFields[0]
-			if len(sshShellFields) > 1 {
-				arg0Args = sshShellFields[1:]
-			}
-		}
-	}
-
-	if arg0 == "" {
-		arg0, err = exec.LookPath("ssh")
-		if err != nil {
-			return err
-		}
+	arg0, arg0Args, err := sshutil.GetSSHArguments()
+	if err != nil {
+		return err
 	}
 
 	sshOpts, err := sshutil.SSHOpts(

cmd/limactl/tunnel.go
@@ -11,7 +11,6 @@ import (
 	"github.com/lima-vm/lima/pkg/freeport"
 	"github.com/lima-vm/lima/pkg/sshutil"
 	"github.com/lima-vm/lima/pkg/store"
-	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -79,30 +78,9 @@ func tunnelAction(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	var (
-		arg0     string
-		arg0Args []string
-	)
-	// FIXME: deduplicate the code clone across `limactl shell` and `limactl tunnel`
-	if sshShell := os.Getenv(envShellSSH); sshShell != "" {
-		sshShellFields, err := shellwords.Parse(sshShell)
-		switch {
-		case err != nil:
-			logrus.WithError(err).Warnf("Failed to split %s variable into shell tokens. "+
-				"Falling back to 'ssh' command", envShellSSH)
-		case len(sshShellFields) > 0:
-			arg0 = sshShellFields[0]
-			if len(sshShellFields) > 1 {
-				arg0Args = sshShellFields[1:]
-			}
-		}
-	}
-
-	if arg0 == "" {
-		arg0, err = exec.LookPath("ssh")
-		if err != nil {
-			return err
-		}
+	arg0, arg0Args, err := sshutil.GetSSHArguments()
+	if err != nil {
+		return err
 	}
 
 	sshOpts, err := sshutil.SSHOpts(

pkg/sshutil/sshutil.go
@@ -21,9 +21,39 @@ import (
 	"github.com/lima-vm/lima/pkg/osutil"
 	"github.com/lima-vm/lima/pkg/store/dirnames"
 	"github.com/lima-vm/lima/pkg/store/filenames"
+	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/cpu"
 )
+
+// Environment variable that allows configuring the command (alias) to execute
+// in place of the 'ssh' executable.
+const EnvShellSSH = "SSH"
+
+func GetSSHArguments() (arg0 string, arg0Args []string, err error) {
+	if sshShell := os.Getenv(EnvShellSSH); sshShell != "" {
+		sshShellFields, err := shellwords.Parse(sshShell)
+		switch {
+		case err != nil:
+			logrus.WithError(err).Warnf("Failed to split %s variable into shell tokens. "+
+				"Falling back to 'ssh' command", EnvShellSSH)
+		case len(sshShellFields) > 0:
+			arg0 = sshShellFields[0]
+			if len(sshShellFields) > 1 {
+				arg0Args = sshShellFields[1:]
+			}
+		}
+	}
+
+	if arg0 == "" {
+		arg0, err = exec.LookPath("ssh")
+		if err != nil {
+			return "", []string{""}, err
+		}
+	}
+
+	return arg0, arg0Args, nil
+}
 
 type PubKey struct {
 	Filename string

lima-vm/lima

cmd/limactl/shell.go
@@ -13,21 +13,16 @@ import (
 	"github.com/lima-vm/lima/pkg/sshutil"
 	"github.com/lima-vm/lima/pkg/store"
 	"github.com/mattn/go-isatty"
-	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
-
-// Environment variable that allows configuring the command (alias) to execute
-// in place of the 'ssh' executable.
-const envShellSSH = "SSH"
 
 const shellHelp = `Execute shell in Lima
 
 lima command is provided as an alias for limactl shell $LIMA_INSTANCE. $LIMA_INSTANCE defaults to "` + DefaultInstanceName + `".
 
 By default, the first 'ssh' executable found in the host's PATH is used to connect to the Lima instance.
-A custom ssh alias can be used instead by setting the $` + envShellSSH + ` environment variable.
+A custom ssh alias can be used instead by setting the $` + sshutil.EnvShellSSH + ` environment variable.
 
 Hint: try --debug to show the detailed logs, if it seems hanging (mostly due to some SSH issue).
 `
@@ -142,28 +137,9 @@ func shellAction(cmd *cobra.Command, args []string) error {
 		)
 	}
 
-	var arg0 string
-	var arg0Args []string
-
-	if sshShell := os.Getenv(envShellSSH); sshShell != "" {
-		sshShellFields, err := shellwords.Parse(sshShell)
-		switch {
-		case err != nil:
-			logrus.WithError(err).Warnf("Failed to split %s variable into shell tokens. "+
-				"Falling back to 'ssh' command", envShellSSH)
-		case len(sshShellFields) > 0:
-			arg0 = sshShellFields[0]
-			if len(sshShellFields) > 1 {
-				arg0Args = sshShellFields[1:]
-			}
-		}
-	}
-
-	if arg0 == "" {
-		arg0, err = exec.LookPath("ssh")
-		if err != nil {
-			return err
-		}
+	arg0, arg0Args, err := sshutil.SSHArguments()
+	if err != nil {
+		return err
 	}
 
 	sshOpts, err := sshutil.SSHOpts(

cmd/limactl/tunnel.go
@@ -11,7 +11,6 @@ import (
 	"github.com/lima-vm/lima/pkg/freeport"
 	"github.com/lima-vm/lima/pkg/sshutil"
 	"github.com/lima-vm/lima/pkg/store"
-	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
@@ -79,30 +78,9 @@ func tunnelAction(cmd *cobra.Command, args []string) error {
 		}
 	}
 
-	var (
-		arg0     string
-		arg0Args []string
-	)
-	// FIXME: deduplicate the code clone across `limactl shell` and `limactl tunnel`
-	if sshShell := os.Getenv(envShellSSH); sshShell != "" {
-		sshShellFields, err := shellwords.Parse(sshShell)
-		switch {
-		case err != nil:
-			logrus.WithError(err).Warnf("Failed to split %s variable into shell tokens. "+
-				"Falling back to 'ssh' command", envShellSSH)
-		case len(sshShellFields) > 0:
-			arg0 = sshShellFields[0]
-			if len(sshShellFields) > 1 {
-				arg0Args = sshShellFields[1:]
-			}
-		}
-	}
-
-	if arg0 == "" {
-		arg0, err = exec.LookPath("ssh")
-		if err != nil {
-			return err
-		}
+	arg0, arg0Args, err := sshutil.SSHArguments()
+	if err != nil {
+		return err
 	}
 
 	sshOpts, err := sshutil.SSHOpts(

pkg/sshutil/sshutil.go
@@ -21,9 +21,39 @@ import (
 	"github.com/lima-vm/lima/pkg/osutil"
 	"github.com/lima-vm/lima/pkg/store/dirnames"
 	"github.com/lima-vm/lima/pkg/store/filenames"
+	"github.com/mattn/go-shellwords"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/sys/cpu"
 )
+
+// Environment variable that allows configuring the command (alias) to execute
+// in place of the 'ssh' executable.
+const EnvShellSSH = "SSH"
+
+func SSHArguments() (arg0 string, arg0Args []string, err error) {
+	if sshShell := os.Getenv(EnvShellSSH); sshShell != "" {
+		sshShellFields, err := shellwords.Parse(sshShell)
+		switch {
+		case err != nil:
+			logrus.WithError(err).Warnf("Failed to split %s variable into shell tokens. "+
+				"Falling back to 'ssh' command", EnvShellSSH)
+		case len(sshShellFields) > 0:
+			arg0 = sshShellFields[0]
+			if len(sshShellFields) > 1 {
+				arg0Args = sshShellFields[1:]
+			}
+		}
+	}
+
+	if arg0 == "" {
+		arg0, err = exec.LookPath("ssh")
+		if err != nil {
+			return "", []string{""}, err
+		}
+	}
+
+	return arg0, arg0Args, nil
+}
 
 type PubKey struct {
 	Filename string