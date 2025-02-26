8e142437a75ee7ad3c5df7086a8dedf1c19f3010
Jan Dubois <jan.dubois@suse.com>
2025-02-26 11:54:26 -0800

    Don't use the result of yaml.Unmarshal after it returns an error
    We calll yaml.UnmarshalWithOptions twice, to detect non-strict YAML, and
    overwrite the result of the first unmarshal with the second one. This seems
    to work, but there is no guarantee that it couldn't have left the struct in
    an undefined state.
    
    Also the prototype should use `*LibYAML` instead of `any` because that is the
    only valid type you should pass in.
    
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- pkg/limayaml/marshal.go
+++ pkg/limayaml/marshal.go
@@ -35,8 +35,8 @@ func unmarshalDisk(dst *Disk, b []byte) error {
 	return yaml.Unmarshal(b, dst)
 }
 
-func Unmarshal(data []byte, v any, comment string) error {
-	if err := yaml.UnmarshalWithOptions(data, v, yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
+func Unmarshal(data []byte, y *LimaYAML, comment string) error {
+	if err := yaml.UnmarshalWithOptions(data, y, yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
 		return fmt.Errorf("failed to unmarshal YAML (%s): %w", comment, err)
 	}
 	// the go-yaml library doesn't catch all markup errors, unfortunately
@@ -44,7 +44,8 @@ func Unmarshal(data []byte, v any, comment string) error {
 	if err := yqutil.ValidateContent(data); err != nil {
 		return fmt.Errorf("failed to unmarshal YAML (%s): %w", comment, err)
 	}
-	if err := yaml.UnmarshalWithOptions(data, v, yaml.Strict(), yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
+	var ignore LimaYAML
+	if err := yaml.UnmarshalWithOptions(data, &ignore, yaml.Strict(), yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
 		logrus.WithField("comment", comment).WithError(err).Warn("Non-strict YAML detected; please check for typos")
 	}
 	return nil
