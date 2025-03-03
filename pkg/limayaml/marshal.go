lima-vm/lima.WIP-UNDER-REPO-TRANSFER

Assemble templates using the new basedOn setting
It allows a template to be constructed by merging values from
one or more base templates together. This merge process will
maintain all comments from both the template and the bases.

The template is assembled before an instance is created, and
only the combined template is stored as lima.yaml in the instance
directory.

There merging semantics are otherwise similar to how lima.yaml
is combined with override.yaml, defaults.yaml, and the builtin
default values.

Signed-off-by: Jan Dubois <jan.dubois@suse.com>

pkg/limayaml/marshal.go
@@ -35,16 +35,32 @@ func unmarshalDisk(dst *Disk, b []byte) error {
 	return yaml.Unmarshal(b, dst)
 }
 
+// unmarshalBaseTemplates unmarshalls `basedOn` which is either a string or a list of strings.
+func unmarshalBaseTemplates(dst *BaseTemplates, b []byte) error {
+	var s string
+	if err := yaml.Unmarshal(b, &s); err == nil {
+		*dst = BaseTemplates{s}
+		return nil
+	}
+	return yaml.Unmarshal(b, dst)
+}
+
 func Unmarshal(data []byte, v interface{}, comment string) error {
-	if err := yaml.UnmarshalWithOptions(data, v, yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
+	opts := []yaml.DecodeOption{
+		yaml.CustomUnmarshaler[BaseTemplates](unmarshalBaseTemplates),
+		yaml.CustomUnmarshaler[Disk](unmarshalDisk),
+	}
+	if err := yaml.UnmarshalWithOptions(data, v, opts...); err != nil {
 		return fmt.Errorf("failed to unmarshal YAML (%s): %w", comment, err)
 	}
-	// the go-yaml library doesn't catch all markup errors, unfortunately
+	// The go-yaml library doesn't catch all markup errors, unfortunately
 	// make sure to get a "second opinion", using the same library as "yq"
 	if err := yqutil.ValidateContent(data); err != nil {
 		return fmt.Errorf("failed to unmarshal YAML (%s): %w", comment, err)
 	}
-	if err := yaml.UnmarshalWithOptions(data, v, yaml.Strict(), yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
+	// Finally log a warning if the YAML file violates the "strict" rules
+	opts = append(opts, yaml.Strict())
+	if err := yaml.UnmarshalWithOptions(data, v, opts...); err != nil {
 		logrus.WithField("comment", comment).WithError(err).Warn("Non-strict YAML is deprecated and will be unsupported in a future version of Lima")
 		// Non-strict YAML is known to be used by Rancher Desktop:
 		// https://github.com/rancher-sandbox/rancher-desktop/blob/c7ea7508a0191634adf16f4675f64c73198e8d37/src/backend/lima.ts#L114-L117

lima-vm/lima

pkg/limayaml/marshal.go
@@ -38,17 +38,33 @@ func unmarshalDisk(dst *Disk, b []byte) error {
 	return yaml.Unmarshal(b, dst)
 }
 
+// unmarshalBaseTemplates unmarshalls `basedOn` which is either a string or a list of strings.
+func unmarshalBaseTemplates(dst *BaseTemplates, b []byte) error {
+	var s string
+	if err := yaml.Unmarshal(b, &s); err == nil {
+		*dst = BaseTemplates{s}
+		return nil
+	}
+	return yaml.Unmarshal(b, dst)
+}
+
 func Unmarshal(data []byte, y *LimaYAML, comment string) error {
-	if err := yaml.UnmarshalWithOptions(data, y, yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
+	opts := []yaml.DecodeOption{
+		yaml.CustomUnmarshaler[BaseTemplates](unmarshalBaseTemplates),
+		yaml.CustomUnmarshaler[Disk](unmarshalDisk),
+	}
+	if err := yaml.UnmarshalWithOptions(data, y, opts...); err != nil {
 		return fmt.Errorf("failed to unmarshal YAML (%s): %w", comment, err)
 	}
-	// the go-yaml library doesn't catch all markup errors, unfortunately
+	// The go-yaml library doesn't catch all markup errors, unfortunately
 	// make sure to get a "second opinion", using the same library as "yq"
 	if err := yqutil.ValidateContent(data); err != nil {
 		return fmt.Errorf("failed to unmarshal YAML (%s): %w", comment, err)
 	}
+	// Finally log a warning if the YAML file violates the "strict" rules
+	opts = append(opts, yaml.Strict())
 	var ignore LimaYAML
-	if err := yaml.UnmarshalWithOptions(data, &ignore, yaml.Strict(), yaml.CustomUnmarshaler[Disk](unmarshalDisk)); err != nil {
+	if err := yaml.UnmarshalWithOptions(data, &ignore, opts...); err != nil {
 		logrus.WithField("comment", comment).WithError(err).Warn("Non-strict YAML detected; please check for typos")
 	}
 	return nil