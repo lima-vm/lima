a047e65b97acecc1bf06904c062b48acf717c2ca
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2025-05-07 17:19:23 +0900

    limactl info: show the detected guest agent paths
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

--- pkg/infoutil/infoutil.go
+++ pkg/infoutil/infoutil.go
@@ -4,19 +4,29 @@
 package infoutil
 
 import (
+	"errors"
+	"io/fs"
+
 	"github.com/lima-vm/lima/pkg/driverutil"
 	"github.com/lima-vm/lima/pkg/limayaml"
 	"github.com/lima-vm/lima/pkg/store/dirnames"
 	"github.com/lima-vm/lima/pkg/templatestore"
+	"github.com/lima-vm/lima/pkg/usrlocalsharelima"
 	"github.com/lima-vm/lima/pkg/version"
+	"github.com/sirupsen/logrus"
 )
 
 type Info struct {
-	Version         string                   `json:"version"`
-	Templates       []templatestore.Template `json:"templates"`
-	DefaultTemplate *limayaml.LimaYAML       `json:"defaultTemplate"`
-	LimaHome        string                   `json:"limaHome"`
-	VMTypes         []string                 `json:"vmTypes"` // since Lima v0.14.2
+	Version         string                       `json:"version"`
+	Templates       []templatestore.Template     `json:"templates"`
+	DefaultTemplate *limayaml.LimaYAML           `json:"defaultTemplate"`
+	LimaHome        string                       `json:"limaHome"`
+	VMTypes         []string                     `json:"vmTypes"`     // since Lima v0.14.2
+	GuestAgents     map[limayaml.Arch]GuestAgent `json:"guestAgents"` // since Lima v1.1.0
+}
+
+type GuestAgent struct {
+	Location string `json:"location"` // since Lima v1.1.0
 }
 
 func GetInfo() (*Info, error) {
@@ -32,6 +42,7 @@ func GetInfo() (*Info, error) {
 		Version:         version.Version,
 		DefaultTemplate: y,
 		VMTypes:         driverutil.Drivers(),
+		GuestAgents:     make(map[limayaml.Arch]GuestAgent),
 	}
 	info.Templates, err = templatestore.Templates()
 	if err != nil {
@@ -40,6 +51,20 @@ func GetInfo() (*Info, error) {
 	info.LimaHome, err = dirnames.LimaDir()
 	if err != nil {
 		return nil, err
+	}
+	for _, arch := range limayaml.ArchTypes {
+		bin, err := usrlocalsharelima.GuestAgentBinary(limayaml.LINUX, arch)
+		if err != nil {
+			if errors.Is(err, fs.ErrNotExist) {
+				logrus.WithError(err).Debugf("Failed to resolve the guest agent binary for %q", arch)
+			} else {
+				logrus.WithError(err).Warnf("Failed to resolve the guest agent binary for %q", arch)
+			}
+			continue
+		}
+		info.GuestAgents[arch] = GuestAgent{
+			Location: bin,
+		}
 	}
 	return info, nil
 }
