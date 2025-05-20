5d7ce17908062071f0f34d7e8555fc06871bf07b
Yichen Zhao <yichenzhao.yz@gmail.com>
2025-05-20 14:35:26 +0800

    fix(cidata): do not overwrite bash in alpine vm
    Signed-off-by: Yichen Zhao <yichenzhao.yz@gmail.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/01-alpine-ash-as-bash.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/01-alpine-ash-as-bash.sh
@@ -7,6 +7,9 @@
 # cloud-init scripts can use `#!/bin/bash` and `set -o pipefail`.
 test -f /etc/alpine-release || exit 0
 
+# If bash already exists, do nothing.
+test -x /bin/bash && exit 0
+
 # Redirect bash to ash (built with CONFIG_ASH_BASH_COMPAT) and hope for the best :)
 # It does support `set -o pipefail`, but not `[[`.
 # /bin/bash can't be a symlink because /bin/ash is a symlink to /bin/busybox
