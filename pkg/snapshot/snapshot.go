635400b9b48b4fc71444d987578b8cc1f60ef437
Ansuman Sahoo <anshumansahoo500@gmail.com>
2025-05-23 16:34:44 +0530

    refactor(BaseDriver, drivers, image-downloader): remove driver.BaseDriver, move drivers and image-downloader to pkg/driver
    Signed-off-by: Ansuman Sahoo <anshumansahoo500@gmail.com>

--- pkg/snapshot/snapshot.go
+++ pkg/snapshot/snapshot.go
@@ -6,35 +6,26 @@ package snapshot
 import (
 	"context"
 
-	"github.com/lima-vm/lima/pkg/driver"
 	"github.com/lima-vm/lima/pkg/driverutil"
 	"github.com/lima-vm/lima/pkg/store"
 )
 
 func Del(ctx context.Context, inst *store.Instance, tag string) error {
-	limaDriver := driverutil.CreateTargetDriverInstance(&driver.BaseDriver{
-		Instance: inst,
-	})
+	limaDriver := driverutil.CreateTargetDriverInstance(inst, 0)
 	return limaDriver.DeleteSnapshot(ctx, tag)
 }
 
 func Save(ctx context.Context, inst *store.Instance, tag string) error {
-	limaDriver := driverutil.CreateTargetDriverInstance(&driver.BaseDriver{
-		Instance: inst,
-	})
+	limaDriver := driverutil.CreateTargetDriverInstance(inst, 0)
 	return limaDriver.CreateSnapshot(ctx, tag)
 }
 
 func Load(ctx context.Context, inst *store.Instance, tag string) error {
-	limaDriver := driverutil.CreateTargetDriverInstance(&driver.BaseDriver{
-		Instance: inst,
-	})
+	limaDriver := driverutil.CreateTargetDriverInstance(inst, 0)
 	return limaDriver.ApplySnapshot(ctx, tag)
 }
 
 func List(ctx context.Context, inst *store.Instance) (string, error) {
-	limaDriver := driverutil.CreateTargetDriverInstance(&driver.BaseDriver{
-		Instance: inst,
-	})
+	limaDriver := driverutil.CreateTargetDriverInstance(inst, 0)
 	return limaDriver.ListSnapshots(ctx)
 }
