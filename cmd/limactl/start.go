535a08047040aa15333129d760269ecb0522e50f
Ansuman Sahoo <anshumansahoo500@gmail.com>
2025-05-24 22:27:38 +0530

    driver(internal): list available built-in drivers
    Signed-off-by: Ansuman Sahoo <anshumansahoo500@gmail.com>

--- cmd/limactl/start.go
+++ cmd/limactl/start.go
@@ -21,6 +21,7 @@ import (
 	"github.com/lima-vm/lima/pkg/limatmpl"
 	"github.com/lima-vm/lima/pkg/limayaml"
 	networks "github.com/lima-vm/lima/pkg/networks/reconcile"
+	"github.com/lima-vm/lima/pkg/registry"
 	"github.com/lima-vm/lima/pkg/store"
 	"github.com/lima-vm/lima/pkg/store/filenames"
 	"github.com/lima-vm/lima/pkg/templatestore"
@@ -32,6 +33,7 @@ func registerCreateFlags(cmd *cobra.Command, commentPrefix string) {
 	flags := cmd.Flags()
 	flags.String("name", "", commentPrefix+"Override the instance name")
 	flags.Bool("list-templates", false, commentPrefix+"List available templates and exit")
+	flags.Bool("list-drivers", false, commentPrefix+"List available drivers and exit")
 	editflags.RegisterCreate(cmd, commentPrefix)
 }
 
@@ -53,6 +55,9 @@ $ limactl create --set='.cpus = 2 | .memory = "2GiB"'
 
 To see the template list:
 $ limactl create --list-templates
+
+To see the drivers list:
+$ limactl create --list-drivers
 
 To create an instance "default" from a local file:
 $ limactl create --name=default /usr/local/share/lima/templates/fedora.yaml
@@ -391,6 +396,14 @@ func createStartActionCommon(cmd *cobra.Command, _ []string) (exit bool, err err
 		w := cmd.OutOrStdout()
 		for _, f := range templates {
 			_, _ = fmt.Fprintln(w, f.Name)
+		}
+		return true, nil
+	} else if listDrivers, err := cmd.Flags().GetBool("list-drivers"); err != nil {
+		return true, err
+	} else if listDrivers {
+		w := cmd.OutOrStdout()
+		for _, d := range registry.DefaultRegistry.List() {
+			_, _ = fmt.Fprintln(w, d)
 		}
 		return true, nil
 	}
