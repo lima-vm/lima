ab6c5dceaa6cd03e4b61d6e453697037ef6b5ba7
Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>
2025-07-17 19:38:01 +0900

    Define "MCP Sandbox Interface" and implement `limactl mcp serve`
    === Interface ===
    
    `pkg/mcp/msi` defines "MCP Sandbox Interface" (tentative)
    that should be reusable for other projects too.
    
    MCP Sandbox Interface defines MCP (Model Context Protocol) tools
    that can be used for reading, writing, and executing local files
    with an appropriate sandboxing technology. The sandboxing technology
    can be more secure and/or efficient than the default tools provided
    by an AI agent.
    
    MCP Sandbox Interface was inspired by Gemini CLI's built-in tools.
    https://github.com/google-gemini/gemini-cli/tree/v0.1.12/docs/tools
    
    === Implementation ===
    
    `limactl mcp serve INSTANCE` launches an MCP server that implements the MCP
    Sandbox Interface.
    
    Use <https://github.com/modelcontextprotocol/inspector>
    to play around with the server.
    
    ```bash
    limactl start default
    brew install mcp-inspector
    mcp-inspector
    ```
    In the web browser,
    - Set `Command` to `limactl`
    - Set `Arguments` to `mcp serve default`
    - Click `▶️Connect`
    
    === Usage with Gemni CLI ===
    
    1. Create `.gemini/extensions/lima/gemini-extension.json` as follows:
    ```json
    {
      "name": "lima",
      "version": "2.0.0",
      "mcpServers": {
        "lima": {
          "command": "limactl",
          "args": [
            "mcp",
            "serve",
            "default"
          ]
        }
      }
    }
    ```
    
    2. Modify `.gemini/settings.json` so as to disable Gemini CLI's built-in tools
    except ones that do not relate to local command execution and file I/O:
    ```json
    {
      "coreTools": ["WebFetchTool", "WebSearchTool", "MemoryTool"]
    }
    ```
    
    Signed-off-by: Akihiro Suda <akihiro.suda.cz@hco.ntt.co.jp>

---
+++ pkg/mcp/toolset/shell.go
@@ -0,0 +1,52 @@
+// SPDX-FileCopyrightText: Copyright The Lima Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package toolset
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"os/exec"
+
+	"github.com/modelcontextprotocol/go-sdk/mcp"
+
+	"github.com/lima-vm/lima/v2/pkg/mcp/msi"
+	"github.com/lima-vm/lima/v2/pkg/ptr"
+)
+
+func (ts *ToolSet) RunShellCommand(ctx context.Context, _ *mcp.ServerSession,
+	params *mcp.CallToolParamsFor[msi.RunShellCommandParams],
+) (*mcp.CallToolResultFor[msi.RunShellCommandResult], error) {
+	args := params.Arguments
+	guestPath, err := ts.TranslateHostPath(args.Directory)
+	if err != nil {
+		return nil, err
+	}
+	cmd := exec.CommandContext(ctx, ts.limactl,
+		append([]string{"shell", "--workdir=" + guestPath, ts.inst.Name},
+			args.Command...)...)
+	var stdout, stderr bytes.Buffer
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+	cmdErr := cmd.Run()
+	res := msi.RunShellCommandResult{
+		Stdout: stdout.String(),
+		Stderr: stderr.String(),
+	}
+	if cmdErr == nil {
+		res.ExitCode = ptr.Of(0)
+	} else {
+		res.Error = cmdErr.Error()
+		if st := cmd.ProcessState; st != nil {
+			res.ExitCode = ptr.Of(st.ExitCode())
+		}
+	}
+	resJ, err := json.Marshal(res)
+	if err != nil {
+		return nil, err
+	}
+	return &mcp.CallToolResultFor[msi.RunShellCommandResult]{
+		Content: []mcp.Content{&mcp.TextContent{Text: string(resJ)}},
+	}, nil
+}
