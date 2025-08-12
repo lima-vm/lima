8231dc8273efa89e6d55c2e403f4ad4104635b96
Anders F Björklund <anders.f.bjorklund@gmail.com>
2025-08-12 19:00:29 +0200

    Set permissions for ssh in wsl2 setup script
    Signed-off-by: Anders F Björklund <anders.f.bjorklund@gmail.com>

--- pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl2-setup.sh
+++ pkg/cidata/cidata.TEMPLATE.d/boot/02-wsl2-setup.sh
@@ -9,10 +9,13 @@
 
 # create user
 useradd -u "${LIMA_CIDATA_UID}" "${LIMA_CIDATA_USER}" -c "${LIMA_CIDATA_COMMENT}" -d "${LIMA_CIDATA_HOME}" -m -s "${LIMA_CIDATA_SHELL}"
+LIMA_CIDATA_GID=$(id -g "${LIMA_CIDATA_USER}")
 mkdir "${LIMA_CIDATA_HOME}"/.ssh/
-chown "${LIMA_CIDATA_USER}" "${LIMA_CIDATA_HOME}"/.ssh/
+chown "${LIMA_CIDATA_UID}:${LIMA_CIDATA_GID}" "${LIMA_CIDATA_HOME}"/.ssh/
+chmod 700 "${LIMA_CIDATA_HOME}"/.ssh/
 cp "${LIMA_CIDATA_MNT}"/ssh_authorized_keys "${LIMA_CIDATA_HOME}"/.ssh/authorized_keys
-chown "${LIMA_CIDATA_USER}" "${LIMA_CIDATA_HOME}"/.ssh/authorized_keys
+chown "${LIMA_CIDATA_UID}:${LIMA_CIDATA_GID}" "${LIMA_CIDATA_HOME}"/.ssh/authorized_keys
+chmod 600 "${LIMA_CIDATA_HOME}"/.ssh/authorized_keys
 
 # add $LIMA_CIDATA_USER to sudoers
 echo "${LIMA_CIDATA_USER} ALL=(ALL) NOPASSWD:ALL" | tee -a /etc/sudoers.d/99_lima_sudoers
