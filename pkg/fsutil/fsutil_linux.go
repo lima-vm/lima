58fc7a8b8720605a0a6edf02054a3fdc36a63c62
Jan Dubois <jan.dubois@suse.com>
2025-08-17 20:44:01 -0700

    For IsNFS check all parent directories up to the root until one exist
    Signed-off-by: Jan Dubois <jan.dubois@suse.com>

--- pkg/fsutil/fsutil_linux.go
+++ pkg/fsutil/fsutil_linux.go
@@ -4,10 +4,28 @@
 package fsutil
 
 import (
+	"errors"
+	"os"
+	"path/filepath"
+
 	"golang.org/x/sys/unix"
 )
 
+// IsNFS checks if the path is on NFS. If the path does not exist yet, it will walk
+// up parent directories until one exists, or it hits '/' or '.'.
+// Any other stat errors will cause IsNFS to fail.
 func IsNFS(path string) (bool, error) {
+	for len(path) > 1 {
+		_, err := os.Stat(path)
+		if err == nil {
+			break
+		}
+		if !errors.Is(err, os.ErrNotExist) {
+			return false, err
+		}
+		path = filepath.Dir(path)
+	}
+
 	var sf unix.Statfs_t
 	if err := unix.Statfs(path, &sf); err != nil {
 		return false, err
