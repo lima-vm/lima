43f502ad85cf388907062b4b27a74a325c60203e
Norio Nomura <norio.nomura@gmail.com>
2025-09-06 10:20:52 +0900

    vz: add SSH over AF_VSOCK
    Since systemd v256 (Ubuntu 24.10), SSH is bound to AF_VSOCK port 22.
    
    https://github.com/systemd/systemd/releases/tag/v256
    > - If the system is run in a VM providing AF_VSOCK support, it automatically
    binds sshd to AF_VSOCK port 22.
    
    https://discourse.ubuntu.com/t/oracular-oriole-release-notes/44878
    > - When sshd is installed on a system, a new systemd generator, systemd-ssh-generator
    binds a socket-activated SSH server to local AF_VSOCK and AF_UNIX sockets under certain conditions.
    
    This changes to delay starting SSH port forwarding until the SSH server on the VM becomes ready.
    If AF_VSOCK port 22 can be connected, start a local SSH port as a proxy for AF_VSOCK port 22,
    instead of starting gvisor's port forwarder.
    
    SSH over VSOCK is faster than SSH over gvisor's port forwarder.
    
    This change is opt-out because it requires VZ and VM with systemd v256+,
    setting `LIMA_SSH_OVER_VSOCK=true` does not mean it works.
    To disable, set `LIMA_SSH_OVER_VSOCK=false`.
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>
    
    Loosen retry interval of connecting to SSH port on VM
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>
    
    Change default timeout to 600 seconds
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>

--- pkg/driver/vz/vm_darwin.go
+++ pkg/driver/vz/vm_darwin.go
@@ -108,7 +108,30 @@ func startVM(ctx context.Context, inst *limatype.Instance, sshLocalPort int) (*v
 					filesToRemove[pidFile] = struct{}{}
 					logrus.Info("[VZ] - vm state change: running")
 
-					err := usernetClient.ConfigureDriver(ctx, inst, sshLocalPort)
+					usernetSSHLocalPort := sshLocalPort
+					useSSHOverVsock := true
+					if envVar := os.Getenv("LIMA_SSH_OVER_VSOCK"); envVar != "" {
+						b, err := strconv.ParseBool(envVar)
+						if err != nil {
+							logrus.WithError(err).Warnf("invalid LIMA_SSH_OVER_VSOCK value %q", envVar)
+						} else {
+							useSSHOverVsock = b
+						}
+					}
+					if !useSSHOverVsock {
+						logrus.Info("LIMA_SSH_OVER_VSOCK is false, skipping detection of SSH server on vsock port")
+					} else if err := usernetClient.WaitOpeningSSHPort(ctx, inst); err == nil {
+						hostAddress := net.JoinHostPort(inst.SSHAddress, strconv.Itoa(usernetSSHLocalPort))
+						if err := wrapper.startVsockForwarder(ctx, 22, hostAddress); err == nil {
+							logrus.Infof("Detected SSH server is listening on the vsock port; changed %s to proxy for the vsock port", hostAddress)
+							usernetSSHLocalPort = 0 // disable gvisor ssh port forwarding
+						} else {
+							logrus.WithError(err).Warn("Failed to detect SSH server on vsock port, falling back to usernet forwarder")
+						}
+					} else {
+						logrus.WithError(err).Warn("Failed to wait for the guest SSH server to become available, falling back to usernet forwarder")
+					}
+					err := usernetClient.ConfigureDriver(ctx, inst, usernetSSHLocalPort)
 					if err != nil {
 						errCh <- err
 					}
