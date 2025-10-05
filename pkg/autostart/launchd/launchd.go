227f150175a068a0458171037d77b1e4884fc1c0
Norio Nomura <norio.nomura@gmail.com>
2025-10-05 17:49:37 +0900

    autostart: Ensure instance is started/stopped by `launchd` or `systemctl`
    If the instance isn’t launched by `launchd`, it won’t be stopped on log out.
    
    This changes to use `launchctl` or `systemctl` to start/stop the instance if it’s registered to autostart.
    
    ## Affected sub commands:
    - `limactl start`
    - `limactl stop`
    - `limactl restart`
    - `limactl edit`
    - `limactl shell`
    
    ## API changes
    
    ### `pkg/autostart`
    - Introduced `AutoStartedIdentifier()`:
      - If not empty, it indicates whether the instance was started by `launchd` or `systemd`.
    - Added `RequestStart()`:
      - Delegates the operation to `launchd` or `systemd`.
    - Added `RequestStop()`:
      - Delegates the operation to `launchd` or `systemd`.
    
    ### `pkg/autostart/launchd`
    - Added `AutoStartedServiceName()`:
      - Uses the XPC_SERVICE_NAME environment variable as the service name.
    - Added `RequestStart()`:
      - Uses `launchctl enable service-target` to avoid failing `bootstrap`.
      - Uses `launchctl bootstrap domain-target plist-path`.
    - Added `RequestStop()`:
      - Uses `launchctl bootout service-target` if the instance is launched by `launchd`.
    - Added `--progress` to the `limactl` option in `io.lima-vm.autostart.INSTANCE.plist`:
      - Required to support `limactl start --progress`.
    
    ### `pkg/autostart/systemd`
    - Added `AutoStartedServiceName()`:
      - Uses `CurrentUnitName()` by `github.com/coreos/go-systemd/v22/util` as the service identifier.
    - Added `RequestStart()`:
      - Uses `systemctl --user start unit-name`.
    - Added `RequestStop()`:
      - Uses `systemctl --user stop unit-name` if the instance is launched by `systemd`.
    - Added `--progress` to the `limactl` option in `lima-vm@INSTANCE.service`:
      - Required to support `limactl start --progress`.
    
    ### `pkg/hostagent`
    - Add `AutoStartedIdentifier` to `Info`.
      - If not empty, it indicates whether the instance was started by `launchd` or `systemd`.
    
    ### `pkg/instance`
    - `StartWithPaths()`:
      - Use `autostart.IsRegistered()` to check if the instance is registered to autostart.
      - If `launchHostAgentForeground` is true, ignore autostart registration.
      - If the instance is registered to autostart, use `autostart.RequestStart()` instead of launching HostAgent.
    - `StopGracefully()`:
      - Use `autostart.RequestStop()`.
    - `Restart()`, `RestartForcibly()`:
      - Use `autostart.IsRegistered()` to skip `networks.Reconcile()` if the instance is registered to autostart.
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>
    
    bump `coreos/go-systemd` to v22.6.0
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>

--- pkg/autostart/launchd/launchd.go
+++ pkg/autostart/launchd/launchd.go
@@ -9,8 +9,11 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"sync"
 
 	"github.com/sirupsen/logrus"
+
+	"github.com/lima-vm/lima/v2/pkg/limatype"
 )
 
 //go:embed io.lima-vm.autostart.INSTANCE.plist
@@ -32,7 +35,7 @@ func EnableDisableService(ctx context.Context, enable bool, instName string) err
 	if !enable {
 		action = "disable"
 	}
-	return launchctl(ctx, action, fmt.Sprintf("gui/%d/%s", os.Getuid(), ServiceNameFrom(instName)))
+	return launchctl(ctx, action, serviceTarget(instName))
 }
 
 func launchctl(ctx context.Context, args ...string) error {
@@ -41,4 +44,45 @@ func launchctl(ctx context.Context, args ...string) error {
 	cmd.Stderr = os.Stderr
 	logrus.Debugf("running command: %v", cmd.Args)
 	return cmd.Run()
+}
+
+// AutoStartedServiceName returns the launchd service name if the instance is started by launchd.
+func AutoStartedServiceName() string {
+	// Assume the instance is started by launchd if XPC_SERVICE_NAME is set and not "0".
+	// To confirm it is actually started by launchd, it needs to use `launch_activate_socket`.
+	// But that requires actual socket activation setup in the plist file.
+	// So we just check XPC_SERVICE_NAME here.
+	if xpcServiceName := os.Getenv("XPC_SERVICE_NAME"); xpcServiceName != "0" {
+		return xpcServiceName
+	}
+	return ""
+}
+
+var domainTarget = sync.OnceValue(func() string {
+	return fmt.Sprintf("gui/%d", os.Getuid())
+})
+
+func serviceTarget(instName string) string {
+	return fmt.Sprintf("%s/%s", domainTarget(), ServiceNameFrom(instName))
+}
+
+func RequestStart(ctx context.Context, inst *limatype.Instance) error {
+	// If disabled, bootstrap will fail.
+	_ = EnableDisableService(ctx, true, inst.Name)
+	if err := launchctl(ctx, "bootstrap", domainTarget(), GetPlistPath(inst.Name)); err != nil {
+		return fmt.Errorf("failed to start the instance %q via launchctl: %w", inst.Name, err)
+	}
+	return nil
+}
+
+func RequestStop(ctx context.Context, inst *limatype.Instance) (bool, error) {
+	logrus.Debugf("AutoStartedIdentifier=%q, ServiceNameFrom=%q", inst.AutoStartedIdentifier, ServiceNameFrom(inst.Name))
+	if inst.AutoStartedIdentifier == ServiceNameFrom(inst.Name) {
+		logrus.Infof("Stopping the instance %q started by launchd", inst.Name)
+		if err := launchctl(ctx, "bootout", serviceTarget(inst.Name)); err != nil {
+			return false, fmt.Errorf("failed to stop the instance %q via launchctl: %w", inst.Name, err)
+		}
+		return true, nil
+	}
+	return false, nil
 }
