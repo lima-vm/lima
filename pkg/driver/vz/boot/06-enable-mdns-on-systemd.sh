4584ef857f3b074b7de3d7af7c2194fd00311668
Norio Nomura <norio.nomura@gmail.com>
2025-11-01 15:45:12 +0900

    pkg/driver/vz: Enable mDNS in guest using `systemd` on macOS host
    This change allows resolving `<guest's hostname>.local` to guest ip by mDNS on macOS host.
    
    e.g.
    ```console
    $ limactl start --name shared-and-vznat --tty=false --network=lima:shared,vzNAT --containerd=none --log-level error
    ...
    $ dns-sd -q lima-shared-and-vznat.local
    DATE: ---Sat 01 Nov 2025---
    15:31:14.555  ...STARTING...
    Timestamp     A/R  Flags         IF  Name                          Type   Class  Rdata
    15:31:14.555  Add  40000003      27  lima-shared-and-vznat.local.  Addr   IN     192.168.64.2
    15:31:14.555  Add  40000002      31  lima-shared-and-vznat.local.  Addr   IN     192.168.105.3
    ^C
    $ ping -c 1 lima-shared-and-vznat.local
    PING lima-shared-and-vznat.local (192.168.64.2): 56 data bytes
    64 bytes from 192.168.64.2: icmp_seq=0 ttl=64 time=0.568 ms
    
    --- lima-shared-and-vznat.local ping statistics ---
    1 packets transmitted, 1 packets received, 0.0% packet loss
    round-trip min/avg/max/stddev = 0.568/0.568/0.568/0.000 ms
    ```
    
    Signed-off-by: Norio Nomura <norio.nomura@gmail.com>

---
+++ pkg/driver/vz/boot/06-enable-mdns-on-systemd.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# SPDX-FileCopyrightText: Copyright The Lima Authors
+# SPDX-License-Identifier: Apache-2.0
+
+set -eux -o pipefail
+
+# Do nothing on OpenRC systems (e.g., Alpine Linux)
+if [[ -f /sbin/openrc-run ]]; then
+	exit 0
+fi
+
+# It depends on systemd-resolved
+command -v systemctl >/dev/null 2>&1 || exit 0
+command -v resolvectl >/dev/null 2>&1 || exit 0
+
+# Configure systemd-resolved to enable mDNS resolution globally
+enable_mdns_conf_path=/etc/systemd/resolved.conf.d/00-lima-enable-mdns.conf
+enable_mdns_conf_content="[Resolve]
+MulticastDNS=yes
+"
+# Create /etc/systemd/resolved.conf.d/00-lima-enable-mdns.conf if its content is different
+if ! diff -q <(echo "${enable_mdns_conf_content}") "${enable_mdns_conf_path}" >/dev/null 2>&1; then
+	mkdir -p "$(dirname "${enable_mdns_conf_path}")"
+	echo "${enable_mdns_conf_content}" >"${enable_mdns_conf_path}"
+	systemctl daemon-reload
+	systemctl restart systemd-resolved.service
+fi
+
+# On Ubuntu, systemd.network's configuration won't work.
+# See: https://unix.stackexchange.com/a/652582
+# So we need to enable mDNS per-link using resolvectl.
+for iface in $(resolvectl status | sed -n -E 's/^Link +[0-9]+ \(([^)]+)\)/\1/p'); do
+	# This setting is volatile and will be lost on reboot, so we need to set it every time
+	resolvectl mdns "${iface}" yes
+done
