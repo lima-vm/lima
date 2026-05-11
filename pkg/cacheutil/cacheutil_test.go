f6319c6c1658e295da16c34d96edc0e09d0c0309
varsha <varsha.narvi.07@gmail.com>
2026-05-11 21:07:15 +0530

    Fix formatting
    Signed-off-by: varsha <varsha.narvi.07@gmail.com>

--- pkg/cacheutil/cacheutil_test.go
+++ pkg/cacheutil/cacheutil_test.go
@@ -11,52 +11,72 @@ import (
 	"github.com/lima-vm/lima/v2/pkg/limatype"
 )
 
-func boolPtr(b bool) *bool { return &b }
+func boolPtr(b bool) *bool {
+	return &b
+}
 
 func TestNerdctlArchive_ReturnsBasename(t *testing.T) {
 	arch := limatype.X8664
+
 	y := &limatype.LimaYAML{
 		Arch: &arch,
 		Containerd: limatype.Containerd{
 			System: boolPtr(true),
 			User:   boolPtr(false),
 			Archives: []limatype.File{
-				{Location: "https://example.com/nerdctl-full-1.0.0-linux-amd64.tar.gz", Arch: limatype.X8664},
+				{
+					Location: "https://example.com/nerdctl-full-1.0.0-linux-amd64.tar.gz",
+					Arch:     limatype.X8664,
+				},
 			},
 		},
 	}
+
 	got := NerdctlArchive(y)
+
 	assert.Equal(t, got, "nerdctl-full-1.0.0-linux-amd64.tar.gz")
 }
 
 func TestNerdctlArchive_ReturnsEmptyWhenContainerdDisabled(t *testing.T) {
 	arch := limatype.X8664
+
 	y := &limatype.LimaYAML{
 		Arch: &arch,
 		Containerd: limatype.Containerd{
 			System: boolPtr(false),
 			User:   boolPtr(false),
 			Archives: []limatype.File{
-				{Location: "https://example.com/nerdctl-full-1.0.0-linux-amd64.tar.gz", Arch: limatype.X8664},
+				{
+					Location: "https://example.com/nerdctl-full-1.0.0-linux-amd64.tar.gz",
+					Arch:     limatype.X8664,
+				},
 			},
 		},
 	}
+
 	got := NerdctlArchive(y)
+
 	assert.Equal(t, got, "")
 }
 
 func TestNerdctlArchive_ReturnsEmptyWhenArchMismatch(t *testing.T) {
 	arch := limatype.X8664
+
 	y := &limatype.LimaYAML{
 		Arch: &arch,
 		Containerd: limatype.Containerd{
 			System: boolPtr(true),
 			User:   boolPtr(false),
 			Archives: []limatype.File{
-				{Location: "https://example.com/nerdctl-full-1.0.0-linux-arm64.tar.gz", Arch: limatype.AARCH64},
+				{
+					Location: "https://example.com/nerdctl-full-1.0.0-linux-arm64.tar.gz",
+					Arch:     limatype.AARCH64,
+				},
 			},
 		},
 	}
+
 	got := NerdctlArchive(y)
+
 	assert.Equal(t, got, "")
 }
